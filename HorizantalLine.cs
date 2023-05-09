e88e12171ed95d0f11717cf8eced06a1484db94b
opilane <opilane@HP-CZC2349HTC>
2023-05-09 11:39:48 +0300

    Add project files.

---
+++ HorizantalLine.cs
@@ -0,0 +1,40 @@
+﻿using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uss
+{
+    class HorizantalLine
+    {
+        List<Point> pList;
+
+        public HorizantalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList= new List<Point>();
+            for(int x = xLeft; x <= xRight; x++) 
+            { 
+               Point p = new Point( x, y, sym );
+                pList.Add( p );
+            }
+            //pList = new List<Point>();
+            //Point p1 = new Point(4, 4, '*');
+            //Point p2 = new Point(5, 4, '*');
+            //Point p3 = new Point(6, 4, '*');
+            //pList.Add(p1);
+            //pList.Add(p2);
+            //pList.Add(p3);
+        }
+                         
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+               p.Draw();
+            }
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace uss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            //p2.x = 4;
+            //p2.y = 5;
+            //p2.sym = '#';
+
+            p2.Draw();
+
+            //List<int> numList= new List<int>();
+            //numList.Add(0);
+            //numList.Add(1);
+            //numList.Add(2);
+
+            //int x = numList[ 0 ];
+            //int y = numList[ 1 ];
+            //int z = numList[ 2 ];
+
+            //foreach(int i in numList )
+            //{
+            //    Console.WriteLine( i );
+            //}
+
+            //numList.RemoveAt( 0 );
+
+            //List<Point> plist = new List<Point>();
+            //plist.Add(p1);
+
+            HorizantalLine line = new HorizantalLine(5, 10, 8, '+');
+            line.Drow();
+
+
+
+            Console.ReadLine();
+        }
+    
+    }
+}
